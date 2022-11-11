00da5506bb8f4066fd5f8960e41b65fb02b8f379
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-11-11 16:24:50 +0900

    DNSEVER 엽습문제3

---
+++ Jinyeop/DNSEVER/Q3.js
@@ -0,0 +1,17 @@
+const n = 3;
+
+function solution(n) {
+  var answer = [];
+  for (var i = 0; i < n; i++) {
+    //
+    answer.push(i + 1);
+    //
+  }
+  console.log(answer);
+  return answer;
+}
+
+solution(n);
+
+// 3 => [1,2,3]
+// 5 => [1,2,3,4,5]
