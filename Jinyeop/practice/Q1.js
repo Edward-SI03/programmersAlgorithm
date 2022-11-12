41b6b377847b927eaa0c5e50f5e1e08fb2fb046f
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-11-12 15:30:39 +0900

    개인 연습 문제 1

---
+++ Jinyeop/practice/Q1.js
@@ -0,0 +1,25 @@
+function solution(a, b, n) {
+  let count = 0;
+  let returnCock = 0;
+  let returnCock2 = 0;
+  let haveCock = n;
+
+  while (a <= haveCock) {
+    returnCock = parseInt(haveCock / a) * b;
+    returnCock2 = parseInt(haveCock / a);
+    count += returnCock;
+
+    if (haveCock % a === 0) {
+      haveCock = returnCock;
+    } else {
+      haveCock = haveCock - returnCock2 * a + returnCock;
+    }
+  }
+
+  console.log(count);
+  return count;
+}
+
+solution(3, 2, 20); // 19
+
+// [3, 2, 20], 36
