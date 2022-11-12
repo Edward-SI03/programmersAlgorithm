1aaa59a921a5e91250e08a87a1044508e68f880e
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-11-12 16:38:51 +0900

    readline 모듈 사용 수정

--- Jinyeop/DNSEVER/Q2.js
+++ Jinyeop/DNSEVER/Q2.js
@@ -1,24 +1,28 @@
 // 문제
 const readline = require("readline");
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
 let input = [];
+let arr = [];
 let string = "*";
 
 rl.on("line", function (line) {
   //   input = line.split(" ");
-  for (let i = 0; i < Number(line); i++) {
+  arr.push(Number(line));
+
+  rl.close();
+}).on("close", function () {
+  //   console.log(Number(input[0]) + Number(input[1]));
+  for (let i = 0; i < arr[0]; i++) {
     input.push(string.repeat(i + 1));
     input.push("\n");
   }
   let solution = input.join("");
   console.log(solution);
 
-  rl.close();
-}).on("close", function () {
-  //   console.log(Number(input[0]) + Number(input[1]));
   process.exit();
 });
