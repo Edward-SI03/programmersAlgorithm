dc53baf1756580510b60d0bf1c7d898b5dbe1f5d
Edward-SI03 <102647711+Edward-SI03@users.noreply.github.com>
2022-12-14 15:44:43 +0900

    서울에서 김서방 찾기

---
+++ Jinyeop/practice/Q19.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12919
+// 서울에서 김서방 찾기
+
+function solution(seoul) {
+  return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
+}
+
+solution(["Jane", "Kim"]);
+
+/*
+["Jane", "Kim"]	"김서방은 1에 있다"
+ */
